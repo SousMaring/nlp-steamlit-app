995033402e095ef5b39a845c6d1ae36cb28fd161
arch-laptop <kmichoud@unistra.fr>
2025-11-20 15:04:53 +0100

    Add text_uppercase function to tools and improve comments for clarity

--- app.py
+++ app.py
@@ -3,18 +3,24 @@ import streamlit as st
 from src.sentiment import analyze_sentiment
 from src.word_count import count_words
 from src.sentence_count import count_sentences
+from src.make_text_uppercase import text_uppercase
 
+# The title
 st.title('Our simple NLP APP to practice GIT')
+# The text area for the user
 user_txt = st.text_area('text to analyze', placeholder='Enter your text here')
 
 # Map options to their handler functions
+# Keys = Name on the button
+# Values = Functions that is executed when button is clicked
 TOOLS = {
+    "Uppercase": text_uppercase,
     "Number of words": count_words,
     "Number of sentences": count_sentences,
     "Sentiment Analysis": analyze_sentiment,
 }
 
-selection = st.pills("Tools", list(TOOLS.keys()), selection_mode="single")
+selection: str|None = st.pills("Tools: ", list(TOOLS.keys()), selection_mode="single")
 
 if selection:
     st.markdown(f"**Selected tool:** {selection}")
@@ -22,7 +28,7 @@ if selection:
     # Call the selected function
     result = TOOLS[selection](user_txt)
 
-    # Handle different result types
+    # Handle different result types // If you need to output something else than pure text, do it here
     if selection == "Sentiment Analysis":
         df = pd.DataFrame({
             'Negative': [result['neg']],
@@ -32,4 +38,5 @@ if selection:
         st.bar_chart(df, horizontal=True, stack=True,
                      color=['#ff4444', '#888888', '#44ff44'])
     else:
+        # It just print the result of your function here.
         st.write(result)
